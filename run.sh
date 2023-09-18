a7b806731394d2263065b0996a17626978588730
crys <tyscrystal@gmail.com>
2023-09-18 19:03:16 +0800

    update bash script

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 # Call fastAPI
-python3 main.py
+!/bin/bash
+
+# Start the Python file (replace 'your_script.py' with your Python file)
+python main.py &
+
+# Get the process ID (PID) of the last background process (Uvicorn)
+uvicorn_pid=$!
+
+# Sleep for a few seconds to allow Uvicorn to start (adjust as needed)
+sleep 5
+
+# Check if Uvicorn is running
+if ps -p $uvicorn_pid > /dev/null; then
+    echo "Uvicorn is running with PID $uvicorn_pid."
+else
+    echo "Uvicorn is not running."
+    exit 1
+fi
